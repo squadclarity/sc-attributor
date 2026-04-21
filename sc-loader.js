a7443ef365d8f38a2afc240d7f93d39d9dac5568
squadclarity <squadclarity@hotmail.com>
2026-04-21 09:16:29 -0500

    Update sc-loader.js

--- sc-loader.js
+++ sc-loader.js
@@ -7,7 +7,7 @@
   if (!client) return;
 
   const sc = document.createElement("script");
-  sc.src = "https://cdn.jsdelivr.net/gh/squadclarity/sc-attributor@v1.1.6/sc-attributor-v1.1.6.js?v=116";
+  sc.src = "https://cdn.jsdelivr.net/gh/squadclarity/sc-attributor@v1.1.6/sc-attributor-v1.1.6.js";
   sc.defer = true;
   sc.setAttribute("data-sc-client", client);
 
